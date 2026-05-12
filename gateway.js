7cb37529a4a3c3b204065fa5525ee1c9a271618a
MaKsim Ryzhikov™ <majezxctoy6799@outlook.com>
2026-05-12 21:49:36 +0300

    Change blocked country from 'US' to 'CH'

--- gateway.js
+++ gateway.js
@@ -6,7 +6,7 @@
     'use strict';
 
     const GATEWAY_CONFIG = {
-        blockedCountries: ['US', 'UK'],
+        blockedCountries: ['CH', 'UK'],
         storageKeys: {
             intendedPage: 'gateway_intended_page',
             gatewayPassed: 'gateway_passed',
